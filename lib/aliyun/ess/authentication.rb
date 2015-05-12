2b314c57c624a67be285b91d06105e91588ea8d4
sunrisela <sunrisela@gmail.com>
2015-05-12 11:15:27 +0800

    Digest::Digest is deprecated; use Digest

--- lib/aliyun/ess/authentication.rb
+++ lib/aliyun/ess/authentication.rb
@@ -26,7 +26,7 @@ module Aliyun
         end
   
         def encoded_canonical
-          digest   = OpenSSL::Digest::Digest.new('sha1')
+          digest   = OpenSSL::Digest.new('sha1')
           b64_hmac = [OpenSSL::HMAC.digest(digest, secret_access_key+'&', sign_string)].pack("m").strip
           url_encode? ? CGI.escape(b64_hmac) : b64_hmac
         end
